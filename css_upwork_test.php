7137b7b8cd24cff712d0829e3cf1d5c294898fa0
Нехай <@dev-srv>
2016-01-31 00:54:21 +0300

    add upwork css tests file and images

--- css_upwork_test.php
+++ css_upwork_test.php
@@ -1,8 +1,27 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
+<head
+style="@page <page-selector-list> {
+orphans:2;
+}"
+</head>
 <?php
 print <<<HTMLOUTPUT
 <h1>Задания из тестов css upwork</h1>
 <p>вопрос 1</p>
-<div style="position:relative;width:200px;height:40px;border: black solid"></div>
+<div style="position:relative;width:200px;height:40px;border: black solid;">1.question 1-clear property.<br><span style="color:red"> Answer is top-wrong </span></div>
+<div style="position:relative;width:200px;height:100px;border: black solid;">2.question 2-property specifies minimum lines of paragraph that must be left at bottom of the page.<br><span style="color:red"> Answer is orphans </span></div>
+<div style="position:relative;width:200px;height:150px;border: black solid;box-shadow:inset 0 0 8px rgba(0,0,0,0.25);">3.question 3-group of properties with inner glow of 25% opacity to a page element.<br> <span style="color:red">Answer is inset 0 0 8px rgba(0,0,0,0.25)</span> </div>
+<div style="position:relative;width:200px;height:150px;border: black solid;">4.question 4-to apply style on every element exept text.<br> <span style="color:red">Answer is input:not(type="text")</span> </div>
+<div style="position:relative;width:200px;height:150px;border: black solid;">5.question 5-what will happen if the pause property will be used as follows?<br> <span style="color:red">Answer is pause-before will be set to 40s and pause after will be set to 60s</span> </div>
+<div style="position:relative;width:200px;height:150px;border: black solid;cursor:none;">6.question 6-what will happen if the cursor property will be set as none?<br> <span style="color:red">Answer is no cursor will be display</span> </div>
+<div style="position:relative;width:200px;height:150px;border: black solid;">7.question 7-if parents elemens display is set to none shildren will not be rendered too<br> <span style="color:red">Answer is true</span> </div>
+<div style="position:relative;width:200px;height:150px;border: black solid;">8.question 8-which of the following properties allow percentages?<br> <span style="color:red">Answer is font size,line-height.</span> </div>
+<div style="position:relative;width:200px;height:150px;border: black solid;">9.question 9-what is the initial size of font-size property?<br> <span style="color:red">Answer is medium.</span> </div>
+<div style="position:relative;width:200px;height:150px;border: black solid;">10.question 10-what is the initial size of font-size property?<br> <span style="color:red">Answer is medium.</span> </div>
+<div style="position:relative;width:200px;height:150px;border: black solid;">11.question 11-box-shadow: 30px 20px 10px 5px black?<br> <span style="color:red">Answer is 2.</span> </div>
+<div style="position:relative;width:200px;height:150px;border: black solid;">12.question what is the initial value of column-fill?<br> <span style="color:red">Answer is balance.</span> </div>
+<div style="position:relative;width:200px;height:150px;border: black solid;border-width:3px;">13.question what is the initial value of column-fill?<br> <span style="color:red">Answer is balance.</span> </div>
+<div style="position:relative;width:200px;height:150px;border: black solid;border-width:3px;">1~.question what will output such code (code about border image gradient red to black)?<br> <span style="color:red">Answer is the border div element will be colored as black-red.</span> </div>
+<div style="position:relative;width:200px;height:150px;border: black solid;border-width:3px;">19.question select all li element but not last one<br> <span style="color:red">Answer is li:not(:last-child).</span> </div>
 HTMLOUTPUT;
 ?>
